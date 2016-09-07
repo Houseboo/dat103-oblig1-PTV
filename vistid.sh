9db1566aa1cc1bcb88bca0a483a5dc433cefcf01
Vegard Valvik <vegard.valvik@gmail.com>
2016-09-07 14:07:26 +0200

    Ferdig med alt, siste opg mangler kommentar

---
+++ vistid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "Hva er hendelsen?"
+
+read hendelse
+
+treff=$(grep -o $hendelse hendelse.logg | wc -l)
+i=1
+sum=0
+tall=0
+
+while [ $i -lt $[treff+1] ];
+do
+	tall=$(grep -m $i $hendelse hendelse.logg | tail -n1 | tr -dc '0-9')
+	sum=$[sum+tall]
+	#echo "Sum = $sum, tall = $tall"
+	let i=i+1
+done
+
+echo $sum
